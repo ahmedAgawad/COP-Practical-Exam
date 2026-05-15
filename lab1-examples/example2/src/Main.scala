a08ec7c537e143beb3eaff7d0b3f6070a5445337
Ahmed Abdelgawad <ahmed.m.elshennawi@gmail.com>
2026-05-15 18:24:30 +0300

    Lab 1 - 2 string interpolation example added

---
+++ lab1-examples/example2/src/Main.scala
@@ -0,0 +1,10 @@
+
+object Main extends App {
+
+  val playerName:String = "Carl";
+  var playerScore = 10;
+  playerScore += 1;
+  println( playerScore + " points for player " + playerName);
+  println(s"$playerScore points for player $playerName");
+}
+
